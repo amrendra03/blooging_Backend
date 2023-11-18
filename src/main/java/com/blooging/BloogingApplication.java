7a218914f78720c1cb33c24b2bf945dd0663ec40
amrendra03 <amrendra9076893685@gmail.com>
2023-11-18 16:17:50 +0530

    Automatically Object Model Mapper

--- src/main/java/com/blooging/BloogingApplication.java
+++ src/main/java/com/blooging/BloogingApplication.java
@@ -1,7 +1,9 @@
 package com.blooging;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BloogingApplication {
@@ -10,5 +12,11 @@ public class BloogingApplication {
 		SpringApplication.run(BloogingApplication.class, args);
 		System.out.println("Started....");
 	}
+
+	@Bean
+	public ModelMapper modelMapper(){
+		return  new ModelMapper();
+	}
+
 
 }
--- src/main/java/com/blooging/services/impl/UserServiceImpl.java
+++ src/main/java/com/blooging/services/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.blooging.services.impl;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,8 @@ public class UserServiceImpl implements UserService {
 
 	@Autowired
 	private UserRepo userRepo;
+	@Autowired
+	 private ModelMapper modelMapper;
 	
 	@Override
 	public UserDto createUser(UserDto userDto) {
@@ -64,24 +67,27 @@ public class UserServiceImpl implements UserService {
 	}
 	
 	public User dtoToUser(UserDto userDto) {
-		
-		User user= new User();
-		user.setId(userDto.getId());
-		user.setName(userDto.getName());
-		user.setEmail(userDto.getEmail());
-		user.setAbout(userDto.getAbout());
-		user.setPassword(userDto.getPassword());
+
+		User user = this.modelMapper.map(userDto,User.class);
+//		User user= new User();
+//		user.setId(userDto.getId());
+//		user.setName(userDto.getName());
+//		user.setEmail(userDto.getEmail());
+//		user.setAbout(userDto.getAbout());
+//		user.setPassword(userDto.getPassword());
 		
 		return user;
 	}
 	
 	public UserDto userToDto(User user) {
-		UserDto userDto = new UserDto();
-		userDto.setId(user.getId());
-		userDto.setName(user.getName());
-		userDto.setEmail(user.getEmail());
-		userDto.setAbout(user.getAbout());
-		userDto.setPassword(user.getPassword());
+
+		UserDto userDto = this.modelMapper.map(user,UserDto.class);
+//		UserDto userDto = new UserDto();
+//		userDto.setId(user.getId());
+//		userDto.setName(user.getName());
+//		userDto.setEmail(user.getEmail());
+//		userDto.setAbout(user.getAbout());
+//		userDto.setPassword(user.getPassword());
 		
 		return userDto;
 	}
